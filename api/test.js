38f20217806aeef4b285c99469e0f7907fbf5809
jiphan <jiphan@umich.edu>
2021-09-28 01:49:04 -0500

    pull titles from subreddit page

--- api/test.js
+++ api/test.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const axios = require('axios')
+const keys = require('../keys.json')
 
 router.get('/', (req, res) => {
     res
@@ -7,9 +9,49 @@ router.get('/', (req, res) => {
         .json({ msg: 'success' })
 })
 
+let reddit_token = ''
+axios.post('https://www.reddit.com/api/v1/access_token',
+    // {
+    //     grant_type: 'password',
+    //     username: keys.reddit_user,
+    //     password: keys.reddit_pass
+    // },
+    `grant_type=password&username=${keys.reddit_user}&password=${keys.reddit_pass}`,
+    {
+        headers: {
+            'User-Agent': `node:test:1.0 by /u/${keys.reddit_user}`
+        },
+        auth: {
+            username: keys.reddit_id,
+            password: keys.reddit_secret
+        }
+    })
+    .then(res => {
+        reddit_token = res.data.access_token
+    })
+    .catch(err => console.log(err))
 
-router.get('/reddit/:id', (req, res) => {
-    //
+router.get('/reddit/:id', async (req, res) => {
+    let d = await axios.get(`https://www.reddit.com/r/${req.params.id}.json`,
+        {
+            //
+        },
+        {
+            headers: {
+                'User-Agent': `node:test:1.0 by /u/${keys.reddit_user}`,
+                Authorization: `bearer ${reddit_token}`
+            }
+        })
+        .catch(err => console.log(err.response.data))
+
+    // if (d) {
+    //     for (let i of d.data.data.children) {
+    //         console.log(i.data.title)
+    //     }
+    // }
+    res
+        .status(200)
+        .json(d.data.data.children.map(i => i.data.title))
 })
 
 module.exports = router
