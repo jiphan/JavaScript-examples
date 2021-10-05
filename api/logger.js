152e6aec68ac2bcbbe08695806eb69bb894692a2
jiphan <jiphan@umich.edu>
2021-10-04 19:13:16 -0500

    write logs to dynamodb

--- api/logger.js
+++ api/logger.js
@@ -1,9 +1,36 @@
 const moment = require('moment')
+const { DynamoDBClient, ListTablesCommand, PutItemCommand } = require('@aws-sdk/client-dynamodb')
+const keys = require('../keys.json')
+
+const client = new DynamoDBClient({
+    credentials: {
+        accessKeyId: keys.aws_id,
+        secretAccessKey: keys.aws_secret
+    },
+    region: 'us-east-2'
+})
 
 function logger(req, res, next) {
-    console.log(
-        `${req.protocol}://${req.get('host')}${req.originalUrl}: ${moment().format()}`
-    )
+    let item = {
+        ip: req.ip,
+        timestamp: moment().format(),
+        path: req.originalUrl,
+        method: req.method,
+        timestamp_unix: moment().unix()
+    }
+    console.log(item.path, item.ip)
+
+    client.send(new PutItemCommand({
+        TableName: 'express-test-log',
+        Item: {
+            ip: { S: item.ip },
+            timestamp_unix: { N: String(item.timestamp_unix) },
+            method: { S: item.method },
+            path: { S: item.path },
+            timestamp: { S: item.timestamp }
+        }
+    }))
+
     next()
 }
 
